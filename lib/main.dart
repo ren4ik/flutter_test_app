2586da6cdad234caa80e11f0be28593c62a740fc
Renat Mavlyutov <mailren82@gmail.com>
2025-08-04 10:17:33 +0000

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'screens/login_screen.dart';
 import 'screens/logs_screen.dart';
 import 'themes/ucell_theme.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
