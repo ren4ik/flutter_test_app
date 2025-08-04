cd9d233787989f7bb8620ba7b862fb554b7edfeb
Renat Mavlyutov <mailren82@gmail.com>
2025-08-04 05:25:18 +0000

    modified:   lib/screens/verification_screen.dart

--- lib/screens/verification_screen.dart
+++ lib/screens/verification_screen.dart
@@ -13,41 +13,33 @@ class VerificationScreen extends StatelessWidget {
     required this.clientHashId,
   });
 
-  Future<void> _startVerification(BuildContext context) async {
-    try {
-      final result = await MyIdClient.start(
-        config: MyIdConfig(
-          sessionId: sessionId,
-          clientHash: clientHash,
-          clientHashId: clientHashId,
-          environment: MyIdEnvironment.TESTING,
-          entryType: MyIdEntryType.IDENTIFICATION,
-        ),
-        iosAppearance: const MyIdIOSAppearance(), // Можно кастомизировать
-      );
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('MyID завершён: ${result.resultStatus.name}'),
-        ),
-      );
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Ошибка MyID: $e'),
-        ),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Проверка клиента')),
+      appBar: AppBar(title: const Text('MyID Verification')),
       body: Center(
         child: ElevatedButton(
-          onPressed: () => _startVerification(context),
-          child: const Text('Запустить MyID'),
+          onPressed: () async {
+            final result = await MyIdClient.start(
+              config: MyIdStartConfig(
+                sessionId: sessionId,
+                clientHash: clientHash,
+                clientHashId: clientHashId,
+                environment: MyIdEnvironment.testing,
+                entryType: MyIdEntryType.identification,
+              ),
+              iosAppearance: const MyIdIOSCustomAppearance(),
+            );
+
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  'MyID завершён: ${result.resultStatus.name}',
+                ),
+              ),
+            );
+          },
+          child: const Text('Начать верификацию'),
         ),
       ),
     );
